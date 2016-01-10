3e62563dfda8dba3abc981232892ff3970824cd4
Henry <henryss@stud.ntnu.no>
2016-01-11 00:44:03 +0100

    Plugin for lang toggle

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,15 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+$(function() {
+   // Hide Language B when the web page loads
+   $('.fransk').hide();
+   $('.toggleNorsk').click(function () {
+       $('.norsk').show();
+       $('.fransk').hide();
+   });
+   $('.toggleFransk').click(function () {
+     $('.fransk').show();
+     $('.norsk').hide();
+   });
+});
